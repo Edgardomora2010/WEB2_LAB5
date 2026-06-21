3632f69a439534a3264cc3ab99599e2691faa059
Edgardo <edgardomora2010@gmail.com>
2026-06-20 22:54:38 -0600

    Lab 5: Validaciones en tareas de usuario (Get,Patch,delete

--- src/main/java/teccr/justdoitcloud/api/TasksController.java
+++ src/main/java/teccr/justdoitcloud/api/TasksController.java
@@ -48,20 +48,79 @@ public class TasksController {
         return taskService.addTaskToUser(user, task);
     }
 
+    /**
+     * Observaciones: Se determina, que este método es que mapea el endpoint:
+     *
+     * /api/v1/users/{userId}/tasks/{id}
+     *
+     * De forma que devuelve una tarea por medio de su id
+     * de tarea, pero en ningún momento se envía como parámetro en id del usuario. Esto quizás
+     * por que las tareas a nivel de base de datos si apuntan a un usuario. Por lo tanto es a
+     * nivel del servicio (taskService) que debería implementarse una validación sobre si la tarea
+     * consultada, corresponde a un usuario, y el usuario existe, etc y devolverse el mensaje indicado
+     * por el enunciado.
+     *  Por otro lado, en la capa de servicio, no se encuentra una forma, comparar una tarea con un usuario
+     *  asociado, sin dejar de enviar el id de usuario para comparar, ya que en el endpoind se está enviando
+     *  pero no se está utilizando, por lo tanto se modifica el método para recibir ambos parámetros.
+     * @param userId
+     * @param id (task)
+     * @return
+     */
     @GetMapping("/{id}")
-    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
-        Optional<Task> taskOpt = taskService.getTaskById(id);
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id, @PathVariable Long userId) {
+        // Se crea método en servicio que pueda recibir 2 argumentos explícitos en la consulta al
+        // endpoint que plantea el enunciado (taskId, userId)
+        Optional<Task> taskOpt = taskService.getValidUserTaskByTaskId(id,userId);
         return taskOpt.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    /**
+     * Observaciones: Para el caso del patchmapping que se encarga de actualizar las tareas relativas a los
+     * usuarios. Se detecta que a diferencia del ejercicio 1, este envía al método del servicio el id de la
+     * tarea, y  un objeto de tipo tarea, en cuyos datos internos se puede obtener el id la tarea, y un usuario
+     * asociado a la misma. Sin embargo sigue siendo necesario enviar el id del usuario, o un objeto usuario al
+     * servicio para establacer una comparación razonable, y que permita evaluar tan correspondencia antes de
+     * aprobar cualquier llamado a método de actualización.
+     *
+     * Por lo antes dicho, en el updatetask también se incluye el usuario, como parámetro en argumentos.
+     * A su vez también en el métdo updatataskfields, del servicio, se incluye el parámetro userId, para que en
+     * dicho método de actualización se pueda establecer una comparación.
+     *
+     * También la respuesta a la vista, se maneja como en el ejericio 1, ya que este método no manejaba
+     * ResponseEntity como si lo hacía el GetMapping, por lo tanto se incluye mismo funcionamiento. En caso
+     * de recibirse un taskOpt válido (tarea actualizada), se envía respuesta http y tarea actualizada, en caso
+     * contrario, se envia el forbidden requerido.
+     */
     @PatchMapping("/{id}")
-    public Task updateTask(@PathVariable Long id, @RequestBody Task task) {
-        return taskService.updateTaskFields(id, task);
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task task, @PathVariable Long userId) {
+
+        Optional<Task> taskOpt =
+                taskService.updateTaskFields(id, task, userId);
+        return taskOpt
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.FORBIDDEN).build());
     }
 
+    /**
+     * Observaciones: Para el caso de enpoint, para borrado, se asume lo mismo que para los casos anteriores
+     * se agrega el parámetro extra de id usuario, para poder establecer comparaciones, en el servicio y
+     * determinar si una tarea específica pertenece a un usuario específico, antes de cualquier intento de
+     * borrado.
+     * Se cambia método void , a función para poner retornar error como en métodos anteriores GET, PATCH
+     * mapping.
+     * @param id
+     * @param userId
+     */
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteTask(@PathVariable Long id) {
-        taskService.deleteTaskById(id);
+    public ResponseEntity<Void> deleteTask(
+            @PathVariable Long id,
+            @PathVariable Long userId) {
+
+        boolean deleted = taskService.deleteTaskById(id, userId);
+
+        if (deleted) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
     }
 }
--- src/main/java/teccr/justdoitcloud/service/TaskService.java
+++ src/main/java/teccr/justdoitcloud/service/TaskService.java
@@ -21,17 +21,21 @@ public class TaskService {
     private final TaskGenerator taskGenerator;
     private final UserRepository userRepository;
     private final TaskArchiver taskArchiver;
+    private final UserService userService;
 
+    // Constructor de clase con inyección de dependencias
     public TaskService(TaskRepository taskRepository,
                        TaskGenerator taskGenerator,
                        UserRepository userRepository,
-                       TaskArchiver taskArchiver) {
+                       TaskArchiver taskArchiver, UserService userService) {
         this.taskRepository = taskRepository;
         this.taskGenerator = taskGenerator;
         this.userRepository = userRepository;
         this.taskArchiver = taskArchiver;
+        this.userService = userService;
     }
 
+    // Devuelve lista de tareas por usuario
     public List<Task> getTasksForUser(User user) {
         return taskRepository.findByUserId(user.getId());
     }
@@ -63,45 +67,138 @@ public class TaskService {
         return taskRepository.save(task);
     }
 
+    // obtiene tarea por id de tarea
     public Optional<Task> getTaskById(Long id) {
+
         if (id == null || id < 0) {
             return Optional.empty();
         }
         return taskRepository.findById(id);
     }
 
-    public Task updateTaskFields(Long id, Task updatedTask) {
-        return taskRepository.findById(id)
-                .map(existingTask -> {
-                    if (updatedTask.getDescription() != null && !updatedTask.getDescription().trim().isEmpty()) {
-                        existingTask.setDescription(updatedTask.getDescription().trim());
-                    }
-                    if (updatedTask.getStatus() != null) {
-                        existingTask.setStatus(updatedTask.getStatus());
-                    }
-                    return taskRepository.save(existingTask);
-                })
-                .orElseThrow(() -> new RuntimeException("Task not found with id: " + id));
+    /**
+     * Internamente debe consultarse la tarea, pero si la misma no pertenece al usuario
+     * identificado por {userId} debe devolverse una respuesta con el cuerpo vacío y el
+     * código 404 (Not Found).
+     */
+    public Optional<Task> getValidUserTaskByTaskId(Long taskId, Long userId) {
+
+        // Se obtiene datos de tarea a partir del id de tarea, utilizando parámetro
+        // consultado en el endpoint (taskId)
+        Optional<Task> task = getTaskById(taskId);
+
+        // Si la tarea no existe o no es vacía
+        if (task.isEmpty()) {
+            return Optional.empty();
+        }
+
+        // se obtiene id de usuario, obtenido de la tarea
+        long taskUserId = task.get().getUserId();
+
+        // Se compara el id de usuario obtenido desde los datos internos del objeto tarea, contra
+        // el id de usuario recibido en argumentos (userId), que también es otro parámetro utilizado
+        // (que se agregó, ya se recibía en consulta al endpoint, pero no se utilizaba en método del
+        // endpoint), si estos coinciden entonces la devolución de datos de la tarea es válida, si no
+        // se debe devolver en el error 404 (no se encontraron datos, dado que no existe un usuario
+        // con ese id, para una tarea con ese id recibido en la consulta al endpoint).
+        if (userId == taskUserId) {
+            // si el id de usuario en argumentos, corresponde al id de usuario, en los datos internos
+            // del objeto tarea, devuelve la tarea
+            return task;
+        }
+
+        return Optional.empty();
     }
 
-    public void deleteTaskById(Long id) {
-        Optional<Task> maybeTask = taskRepository.findById(id);
+    /**
+     * Observaciones se incluyo el parámetro userId, acá hay una complicación ligera, y es que
+     * este método pese a ser de update devuelve una tarea actualizada, por lo que lo mas simple fue
+     * reutilizar el método del ejercicio 1 que ya evaluaba la correspondencia entre 1 user id, y un
+     * user id dentro de un objeto tarea, de forma que solo se devuelva cuando, el método del ejercicio
+     * se cumple, sin embargo se pensó en devolver boolean (true, or false), pero en controller recibe la
+     * tarea por lo tanto, lo más facil para poder manejar desde el controller el codigo de error que plantea
+     * el enunciado es que si no hay correspondencia, se devuelva también un optional vacío. Pero se trató
+     * de realizar cambios mínimos en el servicio para no afectar luego otros funcionamientos, o modificar mas
+     * código dependiente.
+     * @param id
+     * @param updatedTask
+     * @param userId
+     * @return
+     */
+    public Optional<Task> updateTaskFields(Long id, Task updatedTask, Long userId) {
+
+        // Se aprovecha método realizado en ejercicio 1, y que ya devuelve un objeto funcional, si
+        // existe una correspondencia entre el id user y el id user dentro del objeto task
+        Optional<Task> task = getValidUserTaskByTaskId(id, userId);
+
+        // Si la tarea existe o no es vacía
+        if (task.isPresent()) {
+
+            return taskRepository.findById(id)
+
+                    .map(existingTask -> {
+                        if (updatedTask.getDescription() != null
+                                && !updatedTask.getDescription().trim().isEmpty()) {
+                            existingTask.setDescription(updatedTask.getDescription().trim());
+                        }
+
+                        if (updatedTask.getStatus() != null) {
+                            existingTask.setStatus(updatedTask.getStatus());
+                        }
+
+                        return taskRepository.save(existingTask);
+                    });
+        }
+
+        return Optional.empty();
+    }
+
+    // Se incluye párametro de usuario en argumentos de método de borrado para poder evaluar  la
+    // correspondencia entre tareas y usuario, y borrado seguro.
+    public boolean deleteTaskById(Long id, Long userId)
+    {
+        // Se comenta líenea original en método de borrado del servicio
+        //Optional<Task> maybeTask = taskRepository.findById(id);
+
+        // Se cambia linea original anterior, por versión que realiza comprobación entre tareas y
+        // usuarios asociados a tareas
+        // Se aprovecha método realizado en ejercicio 1, y que ya devuelve un objeto funcional, si
+        // existe una correspondencia entre el id user y el id user dentro del objeto task
+
+        // Este método a diferencia del de búsqueda, y actualización es tipo void, por lo que no
+        // devuelve en realidad una respuesta de algo fue borrado, mas allá de un mensaje, pero no
+        // un task o un <optional> de task vacío, por tanto resulta prudente que devuelva un estado
+        // falso o verdadero si algo se realiza o no.
+
+        Optional<Task> maybeTask = getValidUserTaskByTaskId(id, userId);
+
         if (maybeTask.isEmpty()) {
-            throw new RuntimeException("Task not found with id: " + id);
+            return false;
         }
 
         Task task = maybeTask.get();
 
         Optional<User> maybeUser = userRepository.findById(task.getUserId());
+
         maybeUser.ifPresent(user -> {
             try {
-                taskArchiver.archiveTask("tasks-deleted", user, task);
+
+                // se desactiva servicio taskarchiver de otro proyecto externo
+                // taskArchiver.archiveTask("tasks-deleted", user, task);
+
             } catch (Exception ignored) {
-                log.error("Error archiving task with id {} for user id {}: {}", task.getId(), user.getId(), ignored.getMessage());
+
+                // Resulta conveniente dejar log de excepciones por lo que deja igual.
+                log.error("Error archiving task with id {} for user id {}: {}",
+                        task.getId(),
+                        user.getId(),
+                        ignored.getMessage());
             }
         });
 
-        taskRepository.deleteById(id);
+         taskRepository.deleteById(id);
+
+        return true;
     }
 
 }
